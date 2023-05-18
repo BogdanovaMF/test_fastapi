b1a13876182596d2481427a74a4ee719d4b63ac5
BogdanovaMF <122004102+BogdanovaMF@users.noreply.github.com>
2023-05-18 18:56:37 +0300

    fix errors

--- db/create_conn_db.py
+++ db/create_conn_db.py
@@ -1,8 +1,7 @@
 import os
 
+import sqlalchemy.orm
 from sqlalchemy import create_engine
-from sqlalchemy.engine.url import URL
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
@@ -12,25 +11,24 @@ load_dotenv()
 class Postgres_Loader:
     """Класс для подключения к базе данных"""
 
-    config_db = {
-        "drivername": "postgresql",
-        "host": os.environ.get("DB_HOST"),
-        "port": os.environ.get("DB_PORT"),
-        "username": os.environ.get("DB_USER"),
-        "password": os.environ.get("DB_PASSWORD"),
-        "database": os.environ.get("DB_NAME"),
-    }
+    drivername = "postgresql"
+    host = os.environ.get("DB_HOST")
+    port = os.environ.get("DB_PORT")
+    username = os.environ.get("DB_USER")
+    password = os.environ.get("DB_PASSWORD")
+    database = os.environ.get("DB_NAME")
 
     def __init__(self):
         self.engine = None
-        self.model = declarative_base()
+        self.model = sqlalchemy.orm.declarative_base()
+        # self.model = declarative_base()
         self.__create_engine()
         self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
 
     def __create_engine(self) -> None:
         """Создание подключения к PostgreSQL"""
 
-        self.engine = create_engine(URL(**Postgres_Loader.config_db))
+        self.engine = create_engine(f'{self.drivername}://{self.username}:{self.password}@{self.host}:{self.port}/{self.database}')
         self.engine.connect()
 
 
